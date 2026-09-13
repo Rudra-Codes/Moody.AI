4dff7fb57023dbac6b33af79234d2bdbacb04c7f
Khush Kumar Singh <khushm965@gmail.com>
2026-09-13 20:10:38 +0000

    Update model in text generation pipeline

--- test/metrics_original.py
+++ test/metrics_original.py
@@ -33,7 +33,7 @@ batch_df = df # SInce taking all rows
 # Create pipeline
 pipe = pipeline(
     "text-generation",
-    model="Skshackster/gemma3-1b-mental-health-fine-tuned",
+    model="EshAhm/gemma-3-1b-1t-lora-sentiment-analysis",
     device="cuda",
     torch_dtype=torch.bfloat16,
     max_length=None
